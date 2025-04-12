acd669217bba0972aea7c5cd9c1fbe1d01e7404a
ATTADASAIPRAHARSHA <saipraharshaattada@gmail.com>
2025-04-12 12:54:44 +0530

    vercel check

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-import serverless from 'serverless-http';
-import app from './app.js';
 
-export default serverless(app);
+import app from "./app.js";
+
+const startServer = async () => {
+    try {
+        const PORT = 3000;
+        // await mongoose.connect(mongoDBURL);
+        // console.log("✅ MongoDB connected successfully!");
+
+        app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+    } catch (error) {
+        // console.error("❌ MongoDB connection failed:", error);
+        // process.exit(1); // Stop server if DB fails
+    }
+};
+
+startServer();
